807d93d25dd808148d3d019cc69d7162acb7699e
julien <juliencauwet@yahoo.fr>
2019-03-12 12:53:23 +0100

    added context support librarxy in pom

--- biblioback/src/main/java/com/openclassrooms/beans/MailSender.java
+++ biblioback/src/main/java/com/openclassrooms/beans/MailSender.java
@@ -3,6 +3,7 @@ package com.openclassrooms.beans;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
